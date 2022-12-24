f8c1684035be03bbeb57c65d3157a1f68f310a69
AK <114653318+8-AK-8@users.noreply.github.com>
2022-12-24 16:14:30 +0530

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAtXyq_ghFsXeIpIEEWFrciV8lepSgSiOU",
+  authDomain: "chat-web-3c176.firebaseapp.com",
+  databaseURL: "https://chat-web-3c176-default-rtdb.firebaseio.com",
+  projectId: "chat-web-3c176",
+  storageBucket: "chat-web-3c176.appspot.com",
+  messagingSenderId: "615048429265",
+  appId: "1:615048429265:web:bf664390e0d179d54a4ff0"
+};
